aed334f4daa5c1821447fc30daa72554326bdf7f
kodeb8 <andrewjavierortiz@gmail.com>
2022-09-02 12:05:55 -0400

    Init commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import Start from './components/Start'
+
+function App() {
+  return (
+    <div>
+      <Start />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Start.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+function Start() {
+    return (
+        <div className="start">
+            <div className="start--content">
+                <h1>Quizzical</h1>
+                <p>Some description if needed</p>
+                <button>Start quiz</button>
+            </div>
+        </div>
+    )
+}
+
+export default Start
